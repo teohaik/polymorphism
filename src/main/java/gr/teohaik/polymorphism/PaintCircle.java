86c28ca5821a6b73effc13e430d22aff9708c747
chaikal <chaikal@NLIBLAB6-TEACH.anatolia-act.net>
2021-10-20 20:48:05 +0300

    add class diagram

--- src/main/java/gr/teohaik/polymorphism/PaintCircle.java
+++ src/main/java/gr/teohaik/polymorphism/PaintCircle.java
@@ -1,4 +1,4 @@
-package com.mycompany.mavenproject1;
+package gr.teohaik.polymorphism;
 
 /**
  *
--- src/main/java/gr/teohaik/polymorphism/PaintSquare.java
+++ src/main/java/gr/teohaik/polymorphism/PaintSquare.java
@@ -1,5 +1,5 @@
 
-package com.mycompany.mavenproject1;
+package gr.teohaik.polymorphism;
 
 /**
  *
